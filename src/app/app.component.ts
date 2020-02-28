d7df6ea2ec88a3ccbb3f2b562f54c104cceb7a71
abnekhlifa <ala.ben-khlifa@talan.com>
2020-02-28 11:58:35 +0100

    Build, Test , Sonar And Heroku added Full CI / CD Pipeline

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'projetAng';
+export class AppComponent implements OnInit{
+
+  providers;
+
+  constructor(private http: HttpClient) {
+  }
+
+  getProviders() {
+    this.providers = this.http.get(`${environment.baseURL}/providers`);
+  }
+
+  ngOnInit() {
+    this.getProviders();
+  }
+
 }
